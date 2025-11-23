6981d308edd5fbafc906c32960ff8ed143092e3e
Satyamrk <satyamrk18@gmail.com>
2025-11-23 16:03:11 +0530

    Setup for React+Electron_Tailwind

---
+++ src/components/sample.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const sample = () => {
+  return (
+    <div>
+      <h1>i am satyam</h1>
+    </div>
+  );
+};
+export default sample;
---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sample from "../components/Sample";
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-green-700">Welcome to the Home Page</h1>
+      <Sample />
+    </div>
+  );
+};
+
+export default Home;
